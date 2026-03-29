3b245e23c707db367d2a48c900b400425ed41088
YousefMohamed101 <thehappyboy28@gmail.com>
2026-03-30 07:32:31 +0800

    V0.6.3

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,5 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
-  build: {
-    rollupOptions: {
-      // ✅ Prevent wrangler config from being copied to dist
-      external: [],
-    },
-    copyPublicDir: false  // if wrangler.json is in /public
-  }
+  plugins: [react(), cloudflare()]
 })
