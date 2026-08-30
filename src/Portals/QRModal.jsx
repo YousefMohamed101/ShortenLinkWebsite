f98dee7c19aeb301637ae49bfaa013d5587aae22
YousefMohamed101 <thehappyboy28@gmail.com>
2026-08-30 16:02:35 +0800

    V0.10.1

--- src/Portals/QRModal.jsx
+++ src/Portals/QRModal.jsx
@@ -1,16 +1,15 @@
 import './LoginModalcss.css'
-import {useState} from "react";
+
 import QRCode from 'react-qr-code';
 
 
 function QRModal({onClose,url}){
 
-    const [Url, setUrl] = useState(url);
 
     return (
         <div className= "QRLayout">
             <label className={"QRLabel"}>Link QR</label>
-            <QRCode value= {Url} size={256} bgColor="#ffffff" fgColor="#000000" level="H" ></QRCode>
+            <QRCode value= {url} size={256} bgColor="#ffffff" fgColor="#000000" level="H" ></QRCode>
             <button  onClick={onClose}>Close</button>
         </div>
     )
